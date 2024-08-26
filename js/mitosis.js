2d2d73b42f2e1bb536bcba27b683c47d30c40755
Basuthan-hi <157631478+Basuthan-hi@users.noreply.github.com>
2024-08-26 10:49:53 +0530

    layout change

--- js/mitosis.js
+++
@@ -1,9 +0,0 @@
-const popup = document.querySelector('.pop') 
-const active = document.querySelector('.Virchow')
-active.addEventListener('click', () => {
-    if (popup.style.display === 'none') {
-        popup.style.display = 'block';
-    }else {
-        popup.style.display = 'none';
-    }
-})
--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-function open_close(id) {
-    var description = document.getElementsByClassName(id)[0];
-    if (description.id === 'op') {
-        description.id = 'cl';
-    } else {
-        description.id = 'op';
-    }
-}
