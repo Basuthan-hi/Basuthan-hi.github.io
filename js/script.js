341ff708bdbe7658c3c8c8c1860b97a53026bf96
Basuthan-hi <157631478+Basuthan-hi@users.noreply.github.com>
2024-09-23 15:39:06 +0000

    epithelium complete✔️

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-const side_bar  = document.querySelector('aside')
-let side_bar_status = false
-function open_sidebar(){
-    if (side_bar_status){
-        side_bar.setAttribute('class','hide')
-        side_bar_status = false
-    }else{
-        side_bar.setAttribute('class','show')
-        side_bar_status = true
-    }
+function lofu(para){
+    
 }
